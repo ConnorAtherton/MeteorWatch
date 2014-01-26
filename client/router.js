67e2d818fd0e056f0d876b6aefc4d8385c960ae4
Connor Atherton <c.liam.atherton@gmail.com>
2014-01-26 23:19:38 +0000

    Add flashy progress bar

--- client/router.js
+++ client/router.js
@@ -3,6 +3,28 @@ Router.configure({
     layoutTemplate: 'layout'
 });
 
+var filters = {
+  nProgressHook: function () {
+    if (this.ready()) {
+      var time = Math.round(Math.random() * 9) / 10;
+      NProgress.set(time) ;
+      NProgress.done();
+    } else {
+      NProgress.start();
+      this.stop();
+    }
+  },
+
+  resetScroll: function () {
+    var scrollTo = window.currentScroll || 0;
+    $('body').scrollTop(scrollTo);
+    $('body').css("min-height", 0);
+  }
+}
+
+// Use nProgress on every route that has to load a subscription
+  Router.before(filters.nProgressHook);
+
 Router.map(function () {
 
   this.route('all', {
@@ -11,6 +33,7 @@ Router.map(function () {
 
   this.route('posts', {
       path: '/posts',
+      before: filters.nProgressHook
   });
 
   this.route('talks', {
